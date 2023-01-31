095a68744d2cc21ab1835c80accc9faa02637501
Draster <jorlan.jesus@gmail.com>
2023-01-30 23:34:05 -0600

    First commit

---
+++ LearningJavaMF/src/Variables.java
@@ -0,0 +1,15 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Class to store variables 
+		
+		byte edad;
+		edad = 3;
+		 
+
+	}
+
+}
