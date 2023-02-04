b04ff8280ed4fb7ed3a3192974662c51deb4a751
Draster <jorlan.jesus@gmail.com>
2023-02-04 01:19:37 -0600

    Day 2 Completed

---
+++ LearningJavaMF/src/Constants.java
@@ -0,0 +1,17 @@
+
+public class Constants {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		final double number;
+		final int anotherNumber;
+		final char letter;
+	
+		// The final word, is used to indicate the programming language that this is a constant
+	
+
+	} 
+
+}
---
+++ LearningJavaMF/src/Strings.java
@@ -0,0 +1,26 @@
+
+public class Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "Jesus";
+		
+		System.out.println("Mi nombre es" + name);
+		
+		System.out.println("Mi nombre tiene " +  name.length() + " letras en el ");
+		
+		System.out.println("La primera letra de mi nombre es: " + name.charAt(0));
+		
+		
+		int last;
+		
+		last = name.length();
+		
+		System.out.println("La ultima letra de ni nombre es: " + name.charAt(last - 1));
+		
+		
+
+	}
+
+}
